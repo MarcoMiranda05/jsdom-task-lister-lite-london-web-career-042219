4343053407445254f1a38d5d088c1d7d0371a37b
MarcoMiranda05 <marmirand@hotmail.com>
2019-06-03 17:09:29 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		let textField = document.getElementById("new-task-description")
+    let task_text = textField.value
+    e.target.reset(); 
+
+		let taskList = document.getElementById("tasks")
+		let li = document.createElement("li");
+		li.innerText = task_text;
+		taskList.appendChild(li);
+	});
+
 });
